b02d313680f54a0fcf84551ba733e0792a2e9f6f
Kushang Vijay Gharat <kushang81@gmail.com>
2025-10-05 00:34:09 +0530

    str medium

---
+++ Data_Structure/Linear_DS/Strings/Medium/earliest_repeating_char.py
@@ -0,0 +1,35 @@
+# Find the Earliest Repeating Character
+
+# Given a string S of length n, the task is to find the earliest repeated character in it. The earliest repeated character means, the character that occurs more than once and whose second occurrence has the smallest index.
+
+# Example:
+
+# Input: s = "geeksforgeeks" 
+# Output: e 
+# Explanation: e is the first element that repeats
+
+# Input: s = "hello geeks" 
+# Output: l 
+# Explanation: l is the first element that repeats
+
+s = "geeksforgeeks" 
+
+def check_repeating_char(s):
+    n = len(s)
+    res = []
+    for i in range(n):
+        if (len(res) == 0):
+            res.append(s[i])
+            continue
+
+        for j in range(len(res)):
+            print(s[i],res[j])
+            if s[i] != res[j]:
+                res.append(s[i])
+            else:
+                return s[i]
+            
+    return -1
+        
+
+print(check_repeating_char(s), " is the first letter that repeats")

89e6d448465f8e1c713e162f2dfe9556ad495734
Kushang Vijay Gharat <kushang81@gmail.com>
2025-10-19 23:23:56 +0530

    hash collision update

--- Data_Structure/Linear_DS/Hashing/Basics/hash_collision.py
+++ Data_Structure/Linear_DS/Hashing/Basics/hash_collision.py
@@ -1,42 +1,5 @@
 class HashTable:
     def __init__(self):
-        self.MAX_SIZE = 100
-        self.arr = [[] for _ in range(self.MAX_SIZE)] # List Comprehension in Python, for collision 
-    
-    def get_hash(self,key):
-        h = 0
-        for char in key:
-            h += ord(char)
-        return h % self.MAX_SIZE
-
-    def __setitem__(self, key, val):
-        h = self.get_hash(key)
-        found = False
-        for idx, element in enumerate(self.arr[h]):
-            if len(element) == 2 and element[0] == key:
-                self.arr[h][idx] = (key,val)
-                found = True
-                break
-        if not found:
-            self.arr[h].append((key,val))
-    
-    def __getitem__(self,key):
-        h = self.get_hash(key)
-        for element in self.arr[h]:
-            if element[0] == key:
-                return element[1]
-
-    def __delitem__(self, key):
-        h = self.get_hash(key)
-        for idx, element in enumerate(self.arr[h]):
-            if element[0] == key:
-                del self.arr[h][idx]
-                break
-
-
-
-class HashTable:
-    def __init__(self):
         self.MAX_SIZE = 10
         self.arr = [[] for _ in range(self.MAX_SIZE)]
 
